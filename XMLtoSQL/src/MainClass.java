3f6186e6bbc7fdaf3e447062443fbf121fbc205a
HermanMeier <herman.meier@hotmail.com>
2017-03-30 21:24:52 +0300

    XML failiga tegelemine
    Lisasin klassi XMLhandler. Seal on terve hunnik meetodeid mida saab kasutada xml faili manipuleerimiseks.

---
+++ XMLtoSQL/src/MainClass.java
@@ -0,0 +1,15 @@
+import org.jdom2.JDOMException;
+import java.io.IOException;
+
+/**
+ * Created by Herman on 30.03.2017.
+ */
+public class MainClass {
+    public static void main(String[] args) throws JDOMException, IOException {
+        XMLhandler xml = new XMLhandler("biginfo.xml");
+
+        xml.openXML();
+
+        //Mingi kood
+    }
+}
