1152f7483be3d2a704128a01a6a198f4bcfec9be
HermanMeier <herman.meier@hotmail.com>
2017-03-31 13:12:03 +0300

    Lisasin main meetodi koodi, mis tekitab output.txt faili, millest saab luua sql tabeli.

--- XMLtoSQL/src/MainClass.java
+++ XMLtoSQL/src/MainClass.java
@@ -1,5 +1,8 @@
 import org.jdom2.JDOMException;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Herman on 30.03.2017.
@@ -7,9 +10,23 @@ import java.io.IOException;
 public class MainClass {
     public static void main(String[] args) throws JDOMException, IOException {
         XMLhandler xml = new XMLhandler("biginfo.xml");
+        File output = new File("output.txt");
 
         xml.openXML();
 
-        //Mingi kood
+        try (PrintWriter pw = new PrintWriter(output, "UTF-8")) {
+
+            for (int i = 1; i < xml.getNumberOfRows(); i++) {
+
+                for (int j = 0; j < 9; j++) {
+                    if (xml.getRow(i).get(j).toString().equals("")) {
+                        pw.print("NULL"+";");
+                    }
+                    else
+                        pw.print(xml.getRow(i).get(j).toString().replace(" ", "_")+";");
+                }
+                pw.println();
+            }
+        }
     }
 }
